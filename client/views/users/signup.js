d6b045068932435ee861fbbec5c3ac0a183ea666
kvindasAB <kvindas@alivebox.com>
2014-04-18 18:15:06 -0600

    Feature: Signup Captcha Added captcha to signup form and js.

--- client/views/users/signup.js
+++ client/views/users/signup.js
@@ -8,17 +8,27 @@ Template.signup.events({
         throwError(i18n.t('Please fill in all fields'));
         return false;
       }
-      Accounts.createUser({
-          username: username
-        , email: email  
-        , password: password
-      }, function(err){
-        if(err){
-          console.log(err);
-        }else{
-          Router.go('/');
-        }  
+
+
+
+      VisualCaptcha.validateCaptcha(function(){
+            //callback called only on success; if it fails captcha will display a message informing the user why validation has failed
+            //do your form parsing, meteor calls, whatever you would normaly have done
+
+          Accounts.createUser({
+              username: username
+            , email: email
+            , password: password
+          }, function(err){
+            if(err){
+              console.log(err);
+            }else{
+              Router.go('/');
+            }
+          });
+
       });
+
   },
 
   'click #signin': function(){
